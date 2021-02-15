4bc21e6c4f2052dd3627786d3c76f0a84a08d98c
David Marchbanks <ptdave20@gmail.com>
2021-02-15 07:28:01 -0500

    Initial commit

---
+++ csvreader.go
@@ -0,0 +1,193 @@
+package csvreader
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+// TAG represents the available header values for a field
+const TAG string = "csv"
+
+type csvField struct {
+	fieldIndex   int
+	kind         reflect.Kind
+	headerValues []string
+	required     bool
+	rowIndex     int
+}
+
+var falseValues = []string{
+	"f",
+	"false",
+	"0",
+	"inactive",
+}
+
+var trueValues = []string{
+	"t",
+	"true",
+	"1",
+	"active",
+}
+
+type CSVHeader []*csvField
+
+func (c CSVHeader) Length() int {
+	return len(c)
+}
+
+func (c CSVHeader) HeaderValues(index int) []string {
+	return c[index].headerValues
+}
+
+func GetHeader(row []string, data interface{}) CSVHeader {
+	var t reflect.Type
+	v := reflect.ValueOf(data)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+		t = v.Type()
+	} else {
+		t = reflect.TypeOf(data)
+	}
+
+	var header CSVHeader
+
+	for fieldIndex := 0; fieldIndex < t.NumField(); fieldIndex++ {
+		field := t.Field(fieldIndex)
+
+		tagValue := field.Tag.Get(TAG)
+		tagArray := strings.Split(tagValue, ",")
+		for i := range tagArray {
+			if len(tagArray[i]) > 0 {
+				tagArray[i] = strings.TrimSpace(tagArray[i])
+			}
+		}
+
+		// Omit
+		if tagArray[0] == "-" {
+			continue
+		}
+		var newValue = &csvField{
+			fieldIndex:   fieldIndex,
+			kind:         field.Type.Kind(),
+			headerValues: nil,
+			required:     false,
+			rowIndex:     -1,
+		}
+		header = append(header, newValue)
+
+		if len(tagArray[0]) == 0 {
+			tagArray[0] = field.Name
+		}
+
+		for tagArray[len(tagArray)-1] == "required" || tagArray[len(tagArray)-1] == "omitEmpty" {
+			lastField := tagArray[len(tagArray)-1]
+			switch lastField {
+			case "required":
+				newValue.required = true
+				break
+			}
+			tagArray = tagArray[:len(tagArray)-1]
+		}
+
+		newValue.headerValues = tagArray
+
+		// Find the column that matches
+		for headerIndex := range newValue.headerValues {
+			if newValue.rowIndex != -1 {
+				break
+			}
+			for rowCol := range row {
+				if newValue.headerValues[headerIndex] == row[rowCol] {
+					newValue.rowIndex = rowCol
+					break
+				}
+			}
+		}
+	}
+
+	return header
+}
+
+func UnmarshallRow(header CSVHeader, row []string, data interface{}) error {
+	v := reflect.ValueOf(data)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	for _, col := range header {
+		if col.rowIndex == -1 {
+			continue
+		}
+		field := v.Field(col.fieldIndex)
+		switch col.kind {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if len(row[col.rowIndex]) == 0 && col.required {
+				return errors.New(fmt.Sprintf("Column %d is required and does not have a value", col.rowIndex+1))
+			} else if len(row[col.rowIndex]) == 0 && !col.required {
+				field.SetInt(int64(0))
+				continue
+			}
+			value, err := strconv.Atoi(row[col.rowIndex])
+			if err != nil {
+				return errors.New(fmt.Sprintf("Failed to parse column %d to an int: %s", col.rowIndex+1, err))
+			}
+			field.SetInt(int64(value))
+			break
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			if len(row[col.rowIndex]) == 0 && col.required {
+				return errors.New(fmt.Sprintf("Column %d is required and does not have a value", col.rowIndex+1))
+			} else if len(row[col.rowIndex]) == 0 && !col.required {
+				field.SetUint(uint64(0))
+				continue
+			}
+			value, err := strconv.Atoi(row[col.rowIndex])
+			if err != nil {
+				return errors.New(fmt.Sprintf("Failed to parse column %d to an unsigned int: %s", col.rowIndex+1, err))
+			}
+			field.SetUint(uint64(value))
+			break
+		case reflect.String:
+			if len(row[col.rowIndex]) == 0 && col.required {
+				return errors.New(fmt.Sprintf("Column %d is required and does not have a value", col.rowIndex+1))
+			}
+			field.SetString(row[col.rowIndex])
+			break
+		case reflect.Bool:
+			if isOneOfValue(row[col.rowIndex], trueValues) {
+				field.SetBool(true)
+			} else if isOneOfValue(row[col.rowIndex], falseValues) {
+				field.SetBool(false)
+			} else if col.required {
+				return errors.New(fmt.Sprintf("Failed to parse column %d to an boolean", col.rowIndex+1))
+			} else {
+				field.SetBool(false)
+			}
+			break
+		}
+	}
+	return nil
+}
+
+func SetTrueValues(values []string) {
+	trueValues = values
+}
+
+func SetFalseValues(values []string) {
+	falseValues = values
+}
+
+func isOneOfValue(value string, comparison []string) bool {
+	lowerValue := strings.ToLower(value)
+	for index := range comparison {
+		if lowerValue == comparison[index] {
+			return true
+		}
+	}
+	return false
+}
---
+++ csvreader_test.go
@@ -0,0 +1,118 @@
+package csvreader
+
+import "testing"
+
+var valueHeader = []string {
+	"a",
+	"b",
+	"c",
+}
+var valueTest = [][]string {
+	valueHeader,
+	{
+		"1",
+		"2",
+		"true",
+	},
+}
+
+type headerTestAStruct struct {
+	A string `csv:"a"`
+	B string `csv:"b"`
+	C string `csv:"c"`
+}
+
+func TestHeaderA(t *testing.T) {
+	var s headerTestAStruct
+	csvHeader := GetHeader(valueHeader, s)
+	if csvHeader == nil {
+		t.Fail()
+	}
+	if csvHeader.Length() != 3 {
+		t.Fail()
+	}
+	for i := range csvHeader {
+		if headers:=csvHeader.HeaderValues(i); len(headers) != 1 {
+			t.Fail()
+		}
+	}
+}
+func TestUnmarshallA(t *testing.T) {
+	var s headerTestAStruct
+	csvHeader := GetHeader(valueTest[0], s)
+	if csvHeader == nil {
+		t.Fail()
+	}
+
+	if err := UnmarshallRow(csvHeader, valueTest[1], &s); err != nil {
+		t.Logf("Failed test because: %s", err)
+		t.Fail()
+	}
+
+	if s.A != valueTest[1][0] {
+		t.Fail()
+	}
+	if s.B != valueTest[1][1] {
+		t.Fail()
+	}
+	if s.C != valueTest[1][2] {
+		t.Fail()
+	}
+}
+
+type headerTestBStruct struct {
+	A string `csv:"a"`
+	B int `csv:"b"`
+	C bool `csv:"c"`
+}
+
+func TestUnmarshallB(t *testing.T) {
+	var s headerTestBStruct
+	csvHeader := GetHeader(valueTest[0], s)
+	if csvHeader == nil {
+		t.Fail()
+	}
+
+	if err := UnmarshallRow(csvHeader, valueTest[1], &s); err != nil {
+		t.Logf("Failed test because: %s", err)
+		t.Fail()
+	}
+
+	if err := UnmarshallRow(csvHeader, valueTest[1], &s); err != nil {
+		t.Logf("Failed test because: %s", err)
+		t.Fail()
+	}
+
+	if s.A != "1" {
+		t.Fail()
+	}
+
+	if s.B != 2 {
+		t.Fail()
+	}
+
+	if s.C != true {
+		t.Fail()
+	}
+}
+
+func BenchmarkReader(b *testing.B) {
+	// Build data
+	var data = make([][]string, 100001)
+
+	data[0] = valueHeader
+	for i:=1; i<100001; i++ {
+		data[i] = []string {"c", "123", "true"}
+	}
+
+	var s headerTestBStruct
+	csvHeader := GetHeader(data[0], s)
+	b.StartTimer()
+	for i:=1; i<100001; i++ {
+		if err := UnmarshallRow(csvHeader, data[i], &s); err != nil {
+			b.Fail()
+		}
+	}
+
+	b.StopTimer()
+}
